da0f4832d907068e0a560da6e4a6eded6c60ea44
ShaneConroy <93271006+ShaneConroy@users.noreply.github.com>
2023-12-29 20:26:40 +0000

    Began working on player movement
    Added nav mesh to player, no working, still needs fixing,

--- lab-GamesFleadh/Assets/Scripts/Game Scripts/Player Controller.cs
+++ lab-GamesFleadh/Assets/Scripts/Game Scripts/Player Controller.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5f; // Adjust this value to set the movement speed.
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 targetPosition = hit.point;
+
+                Vector3 direction = (targetPosition - transform.position).normalized;
+
+                rb.velocity = new Vector3(direction.x * speed, 0f, direction.z * speed);
+            }
+        }
     }
 }
